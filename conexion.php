d193beb6e618e31cda58a205fceda77741b1e7e3
Cristian Herrera <20163861@itla.edu.do>
2018-12-08 01:34:29 -0400

    conexion

---
+++ conexion.php
@@ -0,0 +1,50 @@
+<?php
+		define("DB_HOST", "77.104.152.148");
+		define("DB_USER", "jhonlyb2_wpcp");
+		define("DB_PASS", "-(0S8ABJ1p");
+		define("DB_NAME", "jhonlyb2_wpcp");
+class Conexion 
+{
+	    
+	 static $instancia=null;
+	 public $dblink;
+	 function __construct()
+	{
+		$this->dblink= mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+		if (!$this->dblink) {
+			echo "Error a conectar con conexion a base de datos";
+		}
+	}
+	 public static function getInstancia()
+	 {
+	 	if(self::$instancia==null)
+	 	{
+	 		self::$instancia= new Conexion();
+	 	}
+	 	return self::$instancia->dblink;
+	 }
+	 public static function Ejecutar($sql)
+	 {
+	 	$dblink=self::getInstancia();
+	 	if (mysqli_query($dblink, $sql)) {
+    return true;
+} else {
+   return false;
+}
+	 }
+	public static function consulta($query)
+	{
+		$dblink=self::getInstancia();
+		$resultado= mysqli_query($dblink,$query);
+		$rs= array();
+		while ($fila= mysqli_fetch_array($resultado)) {
+			$rs[]=$fila;
+		}
+		return $rs;
+	}
+	function __destruct()
+	{
+		mysqli_close($this->dblink);
+	}
+	
+}
