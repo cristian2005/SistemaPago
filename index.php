d1ff03f2478da11ef47772d2540d38036c361759
jhonlyB <jhonlybaez@gmail.com>
2019-01-13 23:07:12 -0400

    merge antony

--- index.php
+++ index.php
@@ -2,16 +2,49 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Metodo de pago</title>
+	<title>Sistema de pago</title>
   <meta http-equiv="Expires" content="0">
   <meta http-equiv="Last-Modified" content="0">
   <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
   <meta http-equiv="Pragma" content="no-cache">
+  <link rel="icon" type="image/png" href="https://www.comunitypowerdom.org/wp-content/uploads/2018/12/LOGO-CARLOS-2.png">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.css"/>
 </head>
 <body>
+
+	<header>
+						<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #e3f2fd;">
+							<a class="navbar-brand" href="https://www.comunitypowerdom.org/">
+								<img src="https://www.comunitypowerdom.org/wp-content/uploads/2018/12/LOGO-CARLOS-2.png" width="60" height="60" alt="logo de comunitypowerdom" >
+							</a>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						    <span class="navbar-toggler-icon"></span>
+						  </button>
+
+							<div class="collapse navbar-collapse" id="navbarSupportedContent">
+										<ul class="navbar-nav ml-auto">
+											<li class="nav-item">
+												<a class="nav-link" href="https://www.comunitypowerdom.org/">Inicio</a>
+											</li>
+
+											<li class="nav-item">
+												<a class="nav-link" href="https://www.comunitypowerdom.org/eventos/">Eventos</a>
+											</li class="nav-item">
+
+											<li class="nav-item">
+												<a class="nav-link" href="https://www.comunitypowerdom.org/my-account/">Mi cuenta</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link" href="https://www.comunitypowerdom.org/blog/">Blog</a>
+											</li>
+										</ul>
+							</div>
+
+							</nav>
+	</header>
+
 	<?php
 define("Regular",100);
 define("Premium",200);
@@ -52,7 +85,7 @@ if (isset($_GET["abono"])) {
  //End datos usuario
 $User=Get_User();
   $IV=Conexion::consulta("
-select concat(wpcp_pc_users.name,' ', wpcp_pc_users.surname) as Miembro,wpcp_pc_users.id 
+select concat(wpcp_pc_users.name,' ', wpcp_pc_users.surname) as Miembro,wpcp_pc_users.id
 from wpcp_pc_users inner join wpcp_pc_user_meta on wpcp_pc_users.id=wpcp_pc_user_meta.user_id
 where  meta_key='codigo-del-referido' and case when substring(categories,15,2)=31 then substring(categories,15,2)=31  else substring(categories,15,2)=30 end and wpcp_pc_user_meta.meta_value in(
 select wpcp_pc_user_meta.meta_value from wpcp_pc_user_meta
@@ -70,7 +103,7 @@ from wpcp_pc_users inner join wpcp_pc_user_meta on wpcp_pc_users.id=wpcp_pc_user
 where   meta_key='codigo-del-referido' and wpcp_pc_user_meta.meta_value in(
 select wpcp_pc_user_meta.meta_value from wpcp_pc_user_meta
 where user_id=".$_GET["iv"]." and meta_key='Codigo'));");
-    $miembro_iv=Conexion::consulta("select concat(wpcp_pc_users.name,' ', wpcp_pc_users.surname) as Miembro,wpcp_pc_users.id 
+    $miembro_iv=Conexion::consulta("select concat(wpcp_pc_users.name,' ', wpcp_pc_users.surname) as Miembro,wpcp_pc_users.id
 from wpcp_pc_users inner join wpcp_pc_user_meta on wpcp_pc_users.id=wpcp_pc_user_meta.user_id
 where   meta_key='codigo-del-referido' and wpcp_pc_user_meta.meta_value in(
 select wpcp_pc_user_meta.meta_value from wpcp_pc_user_meta
@@ -82,18 +115,18 @@ where user_id=".$_GET["iv"]." and meta_key='Codigo');");
   {
     if($total>=$membresia)
           {
-             for ($i=1; $i <=5-$contador ; $i++) { 
+             for ($i=1; $i <=5-$contador ; $i++) {
             $html.='<td></td>';
             }
             $html.='<td><span>Pagado<span></td>';
-           
+
           }
         else
           {
             $resto=$membresia-$total;
       $html.= '<td><a data-toggle="modal" data-id="'.$id_user.'" data-abonado="'.$resto.'" onclick="AdministrarPago(event);" style="color:white;" data-target="#modalpago" class="btn btn-info btn-sm btn-">Pagar</a></td>';
             //Agregando fila vacias
-            for ($i=1; $i <5-$contador ; $i++) { 
+            for ($i=1; $i <5-$contador ; $i++) {
               $html.='<td></td>';
             }
             $html.='<td><span>'.$resto.'<span></td>';
@@ -115,7 +148,7 @@ where user_id=".$_GET["iv"]." and meta_key='Codigo');");
        $html.= '<td><span data-toggle="tooltip" data-placement="top" title="'.date("d/m/y",strtotime($value->Fecha)).'">'.$value->Abono.'</span></td>';
        $total=$total+$value->Abono;
       }
-      
+
       //Asignando el boton de pagar y sus celdas vacias
       switch ($membresia) {
         case 'Regular':
@@ -123,13 +156,13 @@ $html=Organizar_Celdas(Regular,$total,$contador,$html,$id);
           break;
         case 'Premium':
 $html=Organizar_Celdas(Premium,$total,$contador,$html,$id);
-         
+
           break;
           case 'Eventual':
 $html=Organizar_Celdas(Eventual,$total,$contador,$html,$id);
           break;
         default:
-         
+
           break;
       }
     }
@@ -146,7 +179,7 @@ $resto=Premium;
 $resto=Eventual;
           break;
         default:
-         
+
           break;
       }
       $html='<td><a data-toggle="modal" data-id="'.$id.'" data-abonado="'.$resto.'" onclick="AdministrarPago(event);" style="color:white;" data-target="#modalpago" class="btn btn-info btn-sm btn-">Pagar</a></td>
@@ -155,15 +188,28 @@ $resto=Eventual;
      }
       return $html;
   }
-  
-	?>
- 
 
-	<div class="container">
-		<h1 style="text-align: center;">Weekly payment</h1>
-		<br><br>
+	?>
+	<div class="container-fluid" style="background-image: url(https://www.comunitypowerdom.org/wp-content/uploads/2018/10/img-20.jpg) !important;">
+			<div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" >
+	  <div class="col-md-5 p-lg-5 mx-auto my-5">
+	    <h1 class=" font-weight-normal" style="font-size:50px;"><b>Sistema de pago</b></h1>
+	  </div>
+	</div>
+</div>
+
+	<div class="container-fluid">
 		<div class="row">
-			<div class="col-6">
+			<div class="col-12">
+				<h2 class="text-center p-2">Historial de pago</h2>
+				<hr>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-2">
+
+			</div>
+			<div class="col-4">
 				<div class="d-flex flex-row bd-highlight mb-3">
   <div class="p-2 bd-highlight"><h3>LV:</h3></div>
   <div class="p-2 bd-highlight">
@@ -173,7 +219,7 @@ $resto=Eventual;
       <?php }
       else
       {
-        $moderador=Conexion::consulta("select concat(wpcp_pc_users.name,' ', wpcp_pc_users.surname) as Miembro,wpcp_pc_users.id 
+        $moderador=Conexion::consulta("select concat(wpcp_pc_users.name,' ', wpcp_pc_users.surname) as Miembro,wpcp_pc_users.id
 from wpcp_pc_users inner join wpcp_pc_user_meta on wpcp_pc_users.id=wpcp_pc_user_meta.user_id
 where meta_key='Codigo' and wpcp_pc_user_meta.meta_value in(
 select wpcp_pc_user_meta.meta_value from wpcp_pc_user_meta
@@ -185,23 +231,25 @@ where user_id=".$User['id']." and meta_key='codigo-del-referido');");
   </div>
 </div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col">
+			<div class="col-2">
+
+			</div>
+
+			<div class="col-4">
 				<div class="d-flex flex-row bd-highlight mb-3">
   <div class="p-2 bd-highlight"><h3>IV:</h3></div>
   <div class="p-2 bd-highlight">
   	<select id="iv"  class="form-control">
-     <?php 
+     <?php
 if ($User["Categoria"]=="Moderador") {
-    for ($i=0; $i <count($IV) ; $i++) { 
+    for ($i=0; $i <count($IV) ; $i++) {
        ?>
       <option <?php if(isset($_GET["iv"])){ if($_GET["iv"]==$IV[$i]->id) echo "selected";} ?> value="<?php echo $IV[$i]->id;?>"><?php echo $IV[$i]->Miembro;?></option>
-      
+
       <?php }} else if($User["Categoria"]=="Inscriptor") {?>
         <option disabled selected><?php echo $User["Miembro"]?></option>
-  		<?php 
-      } 
+  		<?php
+      }
 else
 {
   echo "<script>alert('Este tipo de usuario no es válido en esta página');window.location='https://www.comunitypowerdom.org/my-account/';</script>";
@@ -213,10 +261,15 @@ else
   </div>
 </div>
 			</div>
-			
+
 		</div>
+</div>
+<br>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
 	<table id="tabla" class="table table-striped">
-  <thead>
+  <thead class="bg-info">
     <tr>
       <th scope="col">Miembros</th>
       <th scope="col">Tarjeta</th>
@@ -231,8 +284,8 @@ else
   <tbody>
      <?php
       if (isset($membresia_iv)) {
-      for ($i=0; $i <count($membresia_iv) ; $i++) { 
-        
+      for ($i=0; $i <count($membresia_iv) ; $i++) {
+
      ?>
    <tr>
     <td><?php echo $miembro_iv[$i]->Miembro?></td>
@@ -243,18 +296,20 @@ else
    <?php }}?>
     <?php
       if (isset($membresia) && $User["Categoria"]=="Inscriptor") {
-      for ($i=0; $i <count($membresia) ; $i++) { 
-        
+      for ($i=0; $i <count($membresia) ; $i++) {
+
      ?>
    <tr>
     <td><?php echo $IV[$i]->Miembro;?></td>
     <td><?php echo $membresia[$i]->membresia;?></td>
     <?php echo Get_Pagos($IV[$i]->id,$membresia[$i]->membresia); $id_iv=$_GET["id"];?>
-   
+
    </tr>
    <?php }}?>
   </tbody>
 </table>
+	</div>
+	</div>
 	</div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -320,12 +375,12 @@ else
       <div class="modal-body">
         <form class="form-inline">
   <div class="form-group mb-2">
-    
+
     <input type="text" id="txtabono" placeholder="Monto a bonar" class="form-control" >
   </div>
   <div class="form-group mx-sm-3 mb-2">
   <a style="color:white;" id="abonar" class="btn btn-primary ">Abonar</a>
-  
+
   </div>
   <a style="color:white;" id="todo" class="btn btn-success mb-2">Pagar todo</a>
 </form>
